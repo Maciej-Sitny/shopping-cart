7283af31a73c748a26f6b821c49aef8d7073b80a
Maciej <maciejsitny@gmail.com>
2022-08-22 22:23:14 +0200

    Add logo next to the 'La Casa'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, BrowserRouter } from "react-router-dom";
-
+import starAndCircle from '../images/starAndCircle-removebg-preview.png'
 export default function Navbar(props) {
     return (
         
@@ -9,7 +9,7 @@ export default function Navbar(props) {
                 <Link className='nav--link' to='/shop'>Shop</Link>
                 <Link className='nav--link' to='/about'>About</Link>
             </div>
-            <Link className='nav--link' to='/home'><h4>La Casa</h4></Link>
+            <Link className='nav--link' to='/home'><div className="lacasa"> <h4>La Casa</h4><img className='hero--title--starAndCircle' src={starAndCircle} alt="" /></div></Link>
             <div className="nav--right">
                 <Link className='nav--link' to='/cart'><i className="fa-solid fa-cart-shopping"></i>(0)</Link>
             </div>
